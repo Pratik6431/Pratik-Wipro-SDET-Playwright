e9fca00d01fe65af30ab69fb46064d0406f5b23b
Pratik6431 <pratikkumarhansda@gmail.com>
2026-05-05 12:57:10 +0530

    Promises

---
+++ Day1/Revision.js
@@ -0,0 +1,19 @@
+// Function
+
+function fruitJuicer(fruit) {
+    return fruit + " juice";
+}
+let glass1 = fruitJuicer("orange");
+console.log(glass1);
+
+let array = [1,2,3,4,5,false];
+console.log(typeof array[0]);
+console.log(array);
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+
+console.log(Array.isArray(array));
