ffee8539b50a8a35bca0e15e87712d51548f2b85
Pratik6431 <pratikkumarhansda@gmail.com>
2026-05-12 11:10:18 +0530

    types, partial, deeppartial, readonly, omit, type narrowing

---
+++ Day10/readonly.ts
@@ -0,0 +1,32 @@
+// const user = {
+//     id: 21,
+//     name: "Pratik",
+//     age: 23
+// } as const;
+
+// // user.name = "Nitish"
+// console.log(user);
+
+
+interface Task {
+    readonly title: string;
+    completed: boolean;
+}
+
+const myTask: Readonly<Task> = {
+    title: "Do Homework",
+    completed: true
+}
+
+const myTask1: Task = {
+    title: "Do Homework",
+    completed: true
+}
+
+// myTask.completed = false;
+
+console.log(myTask);
+console.log(myTask1);
+
+const scores: ReadonlyArray<number> = [10, 20, 30];
+console.log(scores[0]);
